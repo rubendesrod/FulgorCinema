df86fa7440bccb7805067bd2c3ba5e63a1a06070
rubendrClase <rubendr@alumnos.iesgalileo.es>
2023-12-21 19:29:18 +0100

    descargar videos y hacer footer

--- JS/popUps.js
+++ JS/popUps.js
@@ -15,33 +15,11 @@ const interfaz = {
 interfaz.abrirVenom.addEventListener("click", function(){
     interfaz.dialogVenom.showModal();
 })
-/*
+
 interfaz.cerrarVenom.addEventListener("click", function () {
     interfaz.dialogVenom.close();
     interfaz.videoVenom.contentWindow.postMessage(
         '{"event":"command","func":"pauseVideo","args":""}',
         '*'
       );
-  }); */
-
-  interfaz.cerrarVenom.addEventListener("click", function () {
-    console.log("Clic en cerrarVenom");
-    
-    if (interfaz.videoVenom && interfaz.videoVenom.contentWindow) {
-      console.log("contentWindow disponible");
-      
-      setTimeout(function () {
-        console.log("Pausando el video");
-        interfaz.videoVenom.contentWindow.postMessage(
-          '{"event":"command","func":"pauseVideo","args":""}',
-          '*'
-        );
-      }, 100); // Ajusta el tiempo de espera según sea necesario
-    }
-  
-    if (interfaz.dialogVenom) {
-      console.log("Cerrando el diálogo");
-      interfaz.dialogVenom.close();
-    }
   });
-   
